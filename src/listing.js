13e94b3e9204e19ec996edbb42adf1b04c8dabf1
rutumahajan <rutuja.s.mahajan>
2021-05-11 12:43:50 +0530

    User Records are fetched from API and listed

--- src/listing.js
+++ src/listing.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './listing.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import { TableCell, TableRow } from '@material-ui/core';
+import TableHead from '@material-ui/core/TableHead';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteOutlineRoundedIcon from '@material-ui/icons/DeleteOutlineRounded';
+import EditRoundedIcon from '@material-ui/icons/EditRounded';
 const Url = "https://gorest.co.in/public-api/users";
 
-function Listing() {
+function Listing() 
+{
+    const useStyles = makeStyles({
+        root: {
+          background: '#565761',
+          color: 'white',
+          fontWeight:'900'
+        }
+      });
+    
     const [usersData, setUsersData] = useState([]);
   
     useEffect(() => {
@@ -17,19 +33,24 @@ function Listing() {
       
     }, []);
   
-    let content=null
-    if(usersData)
+    let content=null;
+    <TableBody>
     {
+    
       content= usersData.map ((user,key) =>
-      <tr>
-      <td>{user.id}</td>
-      <td>{user.name}</td>
-      <td>{user.email}</td>
-      <td>{user.gender}</td>
-      <td>{user.status}</td>
-      </tr>
-      )
+      <TableRow key = {user.id}>
+                        <TableCell align="center">{user.id}</TableCell>
+                        <TableCell align="center">{user.name}</TableCell>
+                        <TableCell align="center">{user.email}</TableCell>
+                        <TableCell align="center">{user.gender}</TableCell>
+                        <TableCell align="center">{user.status}</TableCell>
+                        <TableCell align="center"><EditRoundedIcon className="icon"/></TableCell>
+                        <TableCell align="center"><DeleteOutlineRoundedIcon className = "icon"/></TableCell>
+     </TableRow>
+      ) 
     }
+    </TableBody>
+    const classes = useStyles()
     return (
     
         <div>
@@ -37,21 +58,22 @@ function Listing() {
           <div class="header">
           <h1>User Registry</h1>
           </div>
-          <div>
-          <table>
-          <tbody>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Gender</th>
-                        <th>Status</th>
-                    </tr>
-                    
-                    {content}
-                    </tbody>
-            </table>
-        
+          <div className="display">
+          <Table stickyHeader className={classes.roundborder}>
+          <TableHead>
+            <TableRow>
+                <TableCell className = {classes.root} align="center">Id</TableCell>
+                <TableCell className = {classes.root} align="center">Name</TableCell>
+                <TableCell className = {classes.root} align="center">Email</TableCell>
+                <TableCell className = {classes.root} align="center">Gender</TableCell>
+                <TableCell className = {classes.root} align="center">Status</TableCell>
+                <TableCell className = {classes.root} align="center"> </TableCell>
+                <TableCell className = {classes.root} align="center"> </TableCell>
+            </TableRow>
+            
+        </TableHead>  
+        {content}
+        </Table>
           </div>
         </div>
       );
